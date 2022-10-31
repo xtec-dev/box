10a3963cf973b14c059d616b36fe59935e42d456
David de Mingo <david@optersoft.com>
2022-10-31 16:54:20 +0100

    Add DriveDownload function

--- vm.ps1
+++ vm.ps1
@@ -5,13 +5,22 @@ if ( $IsWindows) {
     }
 }
 
-#Write-Host(VBoxManage startvm xtec-1 --type headless)
+#  https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-guestcontrol.html
+
 #VBoxManage showvminfo $vmName --machinereadable 
+
+
 
 
 class VM {
     [string]$Name
     [String]$SSH
+
+    [void] Delete() {
+        $this.Stop()
+        Write-Host(vboxmanage unregister $this.name --delete)
+
+    }
 
     [void] Start() {
         Write-Host(vboxmanage startvm $this.Name --type headless)
@@ -28,6 +37,38 @@ class VM {
     }
 }
 
+function DriveDownload {
+    param(
+        [string]$GoogleFileId,
+        [string]$FileDestination)
+
+    # set protocol to tls version 1.2
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    # Download the Virus Warning into _tmp.txt
+    Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=$GoogleFileId" -OutFile "_tmp.txt" -SessionVariable googleDriveSession
+
+    # Get confirmation code from _tmp.txt
+    $searchString = Select-String -Path "_tmp.txt" -Pattern "confirm="
+    $searchString -match "confirm=(?<content>.*)&amp;id="
+    $confirmCode = $matches['content']
+
+    # Delete _tmp.txt
+    Remove-Item "_tmp.txt"
+
+    # Download the real file
+    Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&confirm=${confirmCode}&id=$GoogleFileId" -OutFile $FileDestination -WebSession $googleDriveSession
+}
+
+##### MAIN #####
+
+$ova = "xtec.ova"
+
+if (-not(Test-Path $ova -PathType Leaf)) {
+    Write-Host("Downloading xtec.ova from Google Drive")
+    DriveDownload -GoogleFileId "1UxNLsSvv7eo-M6MmAgadn7m14wEvrMmZ" -FileDestionatoin "xtec.ova"
+}
+
 $vms = New-Object Collections.Generic.List[VM]
 foreach ($i in 1..2) {
     $vm = [VM]::new();
@@ -36,6 +77,7 @@ foreach ($i in 1..2) {
     $vms.Add($vm)
 }
 
+exit
 # vboxmanage list vms
 
 
