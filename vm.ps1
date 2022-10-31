a85616e98370d427cf2b91ca51283fae1f00ca48
David de Mingo <david@optersoft.com>
2022-10-31 09:43:23 +0100

    vm.ps1 hosonly

--- vm.ps1
+++ vm.ps1
@@ -1,12 +1,51 @@
-#
-# Add Virtual Box bin-path to PATH environment variable if necessary:
-#
-if ( $IsWindows &&  (get-command VBoxManage.exe -errorAction silentlyContinue) -eq $null) {
-    $env:path="C:\Program Files\Oracle\VirtualBox;$env:path"
+
+if ( $IsWindows) {
+    if ($null -eq (get-command VBoxManage.exe -errorAction silentlyContinue) ) {
+        $env:path = "C:\Program Files\Oracle\VirtualBox;$env:path"
+    }
 }
 
-Write-Host(VBoxManage startvm xtec-1 --type headless)
+#Write-Host(VBoxManage startvm xtec-1 --type headless)
+#VBoxManage showvminfo $vmName --machinereadable 
 
 
+class VM {
+    [string]$Name
+    [String]$SSH
+}
+
+$vms = New-Object Collections.Generic.List[VM]
+foreach ($i in 1..2) {
+    $vm = [VM]::new();
+    $vm.Name = "xtec-$i"
+    $vm.SSH = "220$i"
+    $vms.Add($vm)
+}
+
+# vboxmanage list vms
+
+# https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-hostonlyif.html
+# https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/network_hostonly.html
+# https://gist.github.com/magnetikonline/46a483cc8c9d0451074642f860d0cac1
+
+# TODO check exists
+
+#vboxmanage list hostonlyifs
+# VBoxManage hostonlyif create
+
+$ifname = "vboxnet0"
+vboxmanage hostonlyif ipconfig $ifname --ip 192.168.56.1 --netmask 255.255.255.0
+vboxmanage dhcpserver modify --ifname $ifname --disable
+
+foreach ($vm in $vms) {
+    vboxmanage modifyvm $vm.Name --nic1 nat
+    vboxmanage modifyvm $vm.Name --nic2 hostonly --hostonlyadapter2 $ifname
+}
 
 # https://www.how2shout.com/how-to/vboxmanage-command-not-found-in-windows-cmd-or-powershell.html
+
+
+
+
+
+
