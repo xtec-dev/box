d3aefd22d59cb1cf8ddd213204e5833c7f23cf78
David de Mingo <david@optersoft.com>
2022-12-11 08:01:09 +0100

    add download

--- src/box.rs
+++ src/box.rs
@@ -1,8 +1,11 @@
 mod google;
 mod hetzner;
+mod vbox;
 
 #[tokio::main]
 async fn main() {
+    vbox::ova_import().await;
+
     if true {
         let result = hetzner::config().await;
         println!("{:?}", result)
