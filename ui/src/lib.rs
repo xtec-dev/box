ca1482d0ee5b9fd345d2597dbaa6a87b7ff78876
David de Mingo <david@optersoft.com>
2022-12-14 09:29:18 +0100

    add ui crate

---
+++ ui/src/lib.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use std::io::stdout;
+use std::sync::{Arc, Mutex};
+use tui::{backend::CrosstermBackend, Terminal};
+
+pub struct App {}
+
+async fn _start_ui(_app: &Arc<Mutex<App>>) -> Result<()> {
+    let stdout = stdout();
+    crossterm::terminal::enable_raw_mode()?;
+
+    let _backend = CrosstermBackend::new(stdout);
+
+    /*
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.hide_cursor()?;
+
+    loop {
+        let app = app.borrow();
+        // Render
+        terminal.draw(|rect| ui::draw(rect, &app))?;
+        // TODO handle inputs here
+    }
+
+    // Restore the terminal and close application
+    terminal.clear()?;
+    terminal.show_cursor()?;
+    crossterm::terminal::disable_raw_mode()?;
+    */
+
+    Ok(())
+}
---
+++ ui/ui.md
@@ -0,0 +1,17 @@
+# ui
+
+https://monkeypatch.io/blog/2021/2021-05-31-rust-tui/
+
+https://github.com/Rigellute/spotify-tui
+
+Windows 10
+Scoop installer
+
+First, make sure scoop installer is on your windows box, for instruction please visit scoop.sh
+
+Then open powershell and run following two commands:
+
+scoop bucket add scoop-bucket https://github.com/Rigellute/scoop-bucket
+scoop install spotify-tui
+
+After that program is available as: spt or spt.exe
